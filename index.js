a09e90c20d3fa6dc806cd638ba8f03e211d1987c
Juho Vepsalainen <bebraw@gmail.com>
2015-03-06 17:11:51 +0200

    Set up a build script
    It outputs `book.pdf`. This will give us a better idea of how many pages
    the book has. Note that the output is a4 and the actual book looks a bit
    smaller.

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+var markdownpdf = require('markdown-pdf');
+
+
+main();
+
+function main() {
+    var mdDocs = [
+        'TOC.md',
+        'acknowledgments.md',
+        'preface.md',
+        'getting-started.md',
+        'build-automation.md',
+        'loading-assets.md',
+        'inlining-assets.md',
+        'introduction-to-react.md',
+        'webpack-and-react.md',
+        'deployment.md',
+        'optimizing-development.md',
+    ];
+    var bookPath = 'book.pdf';
+
+    markdownpdf().concat.from(mdDocs).to(bookPath, function () {
+        console.log('created', bookPath);
+    });
+}
