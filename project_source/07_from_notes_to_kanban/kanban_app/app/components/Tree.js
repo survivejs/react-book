6821b05ee04b2a69e960086028aa070883bd85b1
Juho Vepsalainen <bebraw@gmail.com>
2015-05-07 16:39:12 +0300

    Implement validation for baobab

--- project_source/07_from_notes_to_kanban/kanban_app/app/components/Tree.js
+++ project_source/07_from_notes_to_kanban/kanban_app/app/components/Tree.js
@@ -1,19 +1,31 @@
 import Baobab from 'baobab';
+import types from 'typology';
 
 export default new Baobab({
     lanes: []
   }, {
-    validate: {
-      lanes: [{
-        id: 'number',
-        name: 'string',
-        notes: [
-          {
-            id: 'number',
-            task: 'string',
-          }
-        ]
-      }]
+    validate: (previousState, newState, affectedPaths) => {
+      const schema = {
+        lanes: [{
+          id: 'number',
+          name: 'string',
+          notes: [
+            {
+              id: 'number',
+              task: 'string',
+            }
+          ]
+        }]
+      };
+      const valid = types.check(schema, newState);
+
+      if(!valid) {
+        const err = types.scan(schema, newState);
+
+        console.error(err);
+
+        return new Error(err.error);
+      }
     }
   }
 );
