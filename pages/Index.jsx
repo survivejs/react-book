8527807d3c1594e9f7865d575f3faa8c9b9c3d43
Juho Vepsalainen <bebraw@gmail.com>
2015-05-12 15:46:21 +0300

    Add some initial content to the landing page

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -1,14 +1,33 @@
 var React = require('react');
 
 var Index = React.createClass({
+    displayName: 'Index',
 
-	displayName: 'Index',
+    render: function() {
+        var style = {
+            background: '#bde2bd',
+            //backgroundImage: 'url(/assets/img/front.jpg)'
+        };
 
-	render: function() {
-		return (
-			<div>Index</div>
-		);
-	}
+        return (
+            <div className='post post--front'>
+                <div className='header-image header-image--front' style={style}></div>
+                <div className='post__heading'>
+                    <h1 className='front-header'>SurviveJS</h1>
+                    <h3>Survive the jungles of JavaScript</h3>
+                    <div className='front__buttons'>
+                        <a href='https://github.com/survivejs/webpack_react' className='btn btn--inverted'>View on GitHub</a>
+                    </div>
+                </div>
+                <div className='post__content'>
+                    <h3>What?</h3>
+                    <p>Something</p>
+                    <h3>Why?</h3>
+                    <p>Something</p>
+                </div>
+            </div>
+        );
+    }
 
 });
 
