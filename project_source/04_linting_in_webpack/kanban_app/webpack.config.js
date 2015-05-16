7b209f40c4a8b9a6ec6eda00de259ae703a06b97
Juho Vepsalainen <bebraw@gmail.com>
2015-05-16 12:21:45 +0300

    Add a stub project for linting chapter
    Need to go through this anyway.

--- project_source/04_linting_in_webpack/kanban_app/webpack.config.js
+++ project_source/04_linting_in_webpack/kanban_app/webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 var merge = require('./lib/merge');
 
 var TARGET = process.env.TARGET;
 var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
-  entry: [path.join(ROOT_PATH, 'app/main.js')],
+  entry: [path.join(ROOT_PATH, 'app/main')],
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js',
@@ -23,20 +25,35 @@ var common = {
 var mergeConfig = merge.bind(null, common);
 
 if(TARGET === 'build') {
-  module.exports = mergeConfig({});
+  module.exports = mergeConfig({
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Kanban app',
+            template: path.join(ROOT_PATH, 'app/index.tpl')
+        }),
+    ],
+  });
 }
 
 if(TARGET === 'dev') {
+  var IP = '0.0.0.0';
+  var PORT = 8080;
+
   module.exports = mergeConfig({
-    entry: ['webpack/hot/dev-server'],
-    module: {
-      preLoaders: [
-        {
-          test: /\.jsx?$/,
-          loader: 'eslint-loader',
-          include: path.join(ROOT_PATH, 'app'),
-        }
-      ],
+    ip: IP,
+    port: PORT,
+    entry: [
+      'webpack-dev-server/client?http://' + IP + ':' + PORT,
+      'webpack/hot/dev-server',
+    ],
+    output: {
+      path: __dirname,
+      filename: 'bundle.js',
+      publicPath: '/dev-server/'
     },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+    ]
   });
 }
