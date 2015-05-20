1ecc9754b010f59c20e2d0e1056bee23a518bfe1
Juho Vepsalainen <bebraw@gmail.com>
2015-05-20 22:31:59 +0300

    Fix book link at index

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -23,7 +23,7 @@ var Index = React.createClass({
                     <h1 className='front-header' style={styles.frontHeader}>SurviveJS</h1>
                     <h3>Survive the jungles of JavaScript</h3>
                     <div className='front__buttons'>
-                        <a href='webpack_react' className='btn btn--inverted'>Read the Book</a>
+                        <a href='/webpack_react' className='btn btn--inverted'>Read the Book</a>
                         <a href='https://github.com/survivejs/webpack_react' className='btn btn--inverted'>View on GitHub</a>
                         <a href='https://leanpub.com/survivejs_webpack' className='btn btn--inverted'>Buy the Ebook</a>
                     </div>
