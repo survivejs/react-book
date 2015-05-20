32612333866a6cc742da481b7fdaaefc9218feb2
Juho Vepsalainen <bebraw@gmail.com>
2015-05-20 11:10:36 +0300

    Set up extended site config
    Still need to figure out a nice way to deal with chapter ordering, theme
    linking etc.

--- antwar.config.js
+++ antwar.config.js
@@ -1,4 +1,5 @@
 'use strict';
+var removeMd = require('remove-markdown');
 
 module.exports = {
   output: 'build',
@@ -15,8 +16,32 @@ module.exports = {
     ],
   },
   paths: {
-    webpack_react: function() {
-      return require.context('./manuscript', true, /^\.\/.*\.md$/);
+    webpack_react: {
+      title: function(file) {
+        var title =  file.__content.split('\n')[0];
+        var parts = title.split('#');
+
+        // maybe there's a nicer way to trim markdown?
+        return parts.length > 1 ? parts[1].trim(): '';
+      },
+      date: function(file) {
+        // dates aren't needed
+        return;
+      },
+      preview: function(file) {
+        var content = file.__content.split('\n').slice(1).join('\n');
+
+        var stripped = removeMd(content);
+
+        if(stripped.length > 100) {
+          return stripped.substr(0, 100) + '…';
+        }
+
+        return stripped;
+      },
+      path: function() {
+        return require.context('./manuscript', true, /^\.\/.*\.md$/);
+      }
     }
   }
 };
