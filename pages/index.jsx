c4e9a7d9697975ebae56f15aeed003d1221fcd38
Juho Vepsalainen <bebraw@gmail.com>
2015-06-27 07:39:31 +0300

    Make buy button more prominent at header

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,8 +12,8 @@ var Index = React.createClass({
                     <img style={{margin: '0 auto'}} src='images/title_page_small.jpg' />
                     <div className='front__buttons'>
                         <a href='webpack_react/introduction' className='btn btn--inverted'>Read the Book</a>
+                        <a href='https://leanpub.com/survivejs_webpack' className='btn'>Buy the Ebook</a>
                         <a href='https://github.com/survivejs/webpack_react' className='btn btn--inverted'>View on GitHub</a>
-                        <a href='https://leanpub.com/survivejs_webpack' className='btn btn--inverted'>Buy the Ebook</a>
                     </div>
                 </div>
                 <div className='post post--front'>
