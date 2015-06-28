39227337cb4d86c1b03e6dcd3c3d4cae7452720c
Juho Vepsalainen <bebraw@gmail.com>
2015-06-28 08:49:32 +0300

    Drop redundant buttons from front header
    These should go later to actually motivate usage.

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,11 +10,6 @@ var Index = React.createClass({
                     <h3 className='front-header'>SurviveJS - Webpack and React</h3>
                     <h1 className='front-motto'>Go from zero to Webpack and React hero</h1>
                     <img className='front-cover' src='images/title_page_small.jpg' />
-                    <div className='front__buttons'>
-                        <a href='webpack_react/introduction' className='btn btn--inverted'>Read the Book</a>
-                        <a href='https://leanpub.com/survivejs_webpack' className='btn btn--buy'>Buy the Ebook</a>
-                        <a href='https://github.com/survivejs/webpack_react' className='btn btn--inverted'>View on GitHub</a>
-                    </div>
                 </div>
                 <div className='post post--front'>
                     <div className='post__content' dangerouslySetInnerHTML={{__html: require('./index.md').content}} />
