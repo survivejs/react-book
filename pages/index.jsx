d2b2f2a8f7ec32d9a07302d22106f1557e523088
Juho Vepsalainen <bebraw@gmail.com>
2015-06-29 10:28:40 +0300

    Make landing page header more compact
    Now with cover to left and text to right (natural reading order).

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,10 +11,17 @@ var Index = React.createClass({
                     backgroundImage: 'url(/images/front.jpg)',
                     backgroundPosition: 'top center',
                     width: '100%',
+                    flexDirection: 'row',
+                    paddingTop: '10vh',
+                    paddingBottom: '2vh',
                 }}>
-                    <h3 className='front-header'>SurviveJS - Webpack and React</h3>
-                    <h1 className='front-motto'>Go from zero to Webpack and React hero</h1>
-                    <img className='front-cover' src='images/title_page_small.jpg' />
+                    <div className='front-cover-wrapper'>
+                        <img className='front-cover' src='images/title_page_small.jpg' />
+                    </div>
+                    <div className='front-text-wrapper'>
+                        <h3 className='front-header'>SurviveJS - Webpack and React</h3>
+                        <h1 className='front-motto'>Go from zero to Webpack and React hero</h1>
+                    </div>
                 </div>
                 <div className='post post--front'>
                     <div className='post__content' dangerouslySetInnerHTML={{__html: require('./index.md').content}} />
