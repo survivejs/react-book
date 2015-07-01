e64aec0af0cfa29a739a953277da96b8edf4a1f9
Juho Vepsalainen <bebraw@gmail.com>
2015-07-01 16:50:25 +0300

    Redo header
    Looks a bit better now and tagline makes more sense. Less cliché.

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,14 +9,13 @@ var Index = React.createClass({
                 <div className='front__heading' style={{
                     backgroundSize: 'cover',
                     backgroundImage: 'url(/images/front.jpg)',
-                    backgroundPosition: 'top center',
+                    backgroundPosition: '0px 42%',
                     width: '100%',
                     flexDirection: 'row',
-                    paddingBottom: '2vh',
                 }}>
                     <div className='front-text-wrapper'>
-                        <h3 className='front-header'>SurviveJS - Webpack and React</h3>
-                        <h1 className='front-motto'>Go from zero to Webpack and React hero</h1>
+                        <h1 className='front-header'>SurviveJS - Webpack and React</h1>
+                        <h3 className='front-motto'>From apprentice to master</h3>
 
                         <div className='front__buttons'>
                             <a className='btn btn--inverted' href='/webpack_react/introduction'>Read the book</a>
