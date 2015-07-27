ad6ea67d73539020195ce840b01ab44a6b9e1f67
Jesús Rodríguez Rodríguez <Foxandxss@gmail.com>
2015-07-27 13:41:50 +0200

    Update build_all.sh to include NPM dependencies
    It defeats the purpose if an user needs to go to each project to install the dependencies. Useful is someone wants to try the `site` repo.

--- project_source/build_all.sh
+++ project_source/build_all.sh
@@ -4,6 +4,8 @@ for d in */; do
     APP_DIR="$d/kanban_app"
     if [ -d "$APP_DIR" ]; then
         pushd "$APP_DIR"
+        echo "Installing dependencies for $d"
+        npm install
         echo "Building $d"
         npm run build
         popd
