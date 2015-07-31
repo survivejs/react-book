a579f4ed4f3ea7e1e1214bf2f68418056e92fbcc
Juho Vepsalainen <bebraw@gmail.com>
2015-07-31 11:03:38 +0300

    Drop linting example
    This doesn't add much to the whole given it's just config. If there's a
    good reason, this can be added back later.

--- project_source/09_linting_in_webpack/kanban_app/app/components/ItemTypes.js
+++
@@ -1,3 +0,0 @@
-export default {
-  NOTE: 'note'
-};
--- project_source/09_linting_in_webpack/kanban_app/app/libs/alt.js
+++
@@ -1,7 +0,0 @@
-import Alt from 'alt';
-//import chromeDebug from 'alt/utils/chromeDebug';
-
-const alt = new Alt();
-//chromeDebug(alt);
-
-export default alt;
