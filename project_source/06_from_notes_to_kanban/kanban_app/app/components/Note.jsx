b7b0f3d18884624684e3e2356ee75097ffd78b9e
Juho Vepsalainen <bebraw@gmail.com>
2015-12-11 15:42:27 +0200

    from notes to kanban - Add missing file
    `Note.jsx` is needed now that `Editable.jsx` doesn't get renamed.

---
+++ project_source/06_from_notes_to_kanban/kanban_app/app/components/Note.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
+
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    return <input type="text"
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  }
+  renderNote = () => {
+    const onDelete = this.props.onDelete;
+
+    return (
+      <div onClick={this.edit}>
+        <span className="task">{this.props.task}</span>
+        {onDelete ? this.renderDelete() : null }
+      </div>
+    );
+  }
+  renderDelete = () => {
+    return <button className="delete" onClick={this.props.onDelete}>x</button>;
+  }
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  }
+  checkEnter = (e) => {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+  finishEdit = (e) => {
+    this.props.onEdit(e.target.value);
+
+    this.setState({
+      editing: false
+    });
+  }
+}
