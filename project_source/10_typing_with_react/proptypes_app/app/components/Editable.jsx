7462df5a12d9f2f18bfc8ee3598221572ff53f4b
David Gomez <dg.dave@gmail.com>
2015-12-30 17:37:37 -0500

    refactor more advanced chapters to use placeholder

--- project_source/10_typing_with_react/proptypes_app/app/components/Editable.jsx
+++ project_source/10_typing_with_react/proptypes_app/app/components/Editable.jsx
@@ -13,7 +13,7 @@ class Editable extends React.Component {
   renderEdit = () => {
     return <input type="text"
       autoFocus={true}
-      defaultValue={this.props.value}
+      placeholder={this.props.value}
       onBlur={this.finishEdit}
       onKeyPress={this.checkEnter} />;
   }
