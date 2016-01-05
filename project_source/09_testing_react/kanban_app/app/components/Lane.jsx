3af9926b872192b980d5485eec05e6243a64f9e2
Juho Vepsalainen <bebraw@gmail.com>
2016-01-05 14:07:52 +0200

    testing with react - Drop debug prints

--- project_source/09_testing_react/kanban_app/app/components/Lane.jsx
+++ project_source/09_testing_react/kanban_app/app/components/Lane.jsx
@@ -77,10 +77,10 @@ export default class Lane extends React.Component {
       LaneActions.delete(id);
     }
   }
-  activateLaneEdit(id) {console.log('activate lane edit');
+  activateLaneEdit(id) {
     LaneActions.update({id, editing: true});
   }
-  activateNoteEdit(id) {console.log('activate note edit');
+  activateNoteEdit(id) {
     NoteActions.update({id, editing: true});
   }
 }
